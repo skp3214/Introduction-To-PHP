3894dbf6716f7863ad0a0ca5f6967abb281257b0
Sachin Prajapati <skprajapati3214@gmail.com>
2024-10-21 10:14:22 +0530

    table creation

---
+++ database/TableCreation.php
@@ -0,0 +1,29 @@
+<?php 
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="fruit";
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+    die("Connection Failed: ".$conn->connect_error);
+}
+
+echo"Connected Successfully";
+
+$sql="CREATE TABLE IF NOT EXISTS fruits(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    color VARCHAR(30) NOT NULL,
+    price DECIMAL(5,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+
+if($conn->query($sql)===TRUE){
+    echo"Table created successfully";
+}else{
+    echo"Error creating table: ".$conn->error;
+}
+$conn->close();
